416fc8e461e55097e108ea09341801d2df65bb9b
Sekwah <contact@sekwah.com>
2021-08-02 23:27:07 +0100

    feat: Set up base mod

---
+++ src/main/java/com/sekwah/sekcdevtools/SekCDevTools.java
@@ -0,0 +1,12 @@
+package com.sekwah.sekcdevtools;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(SekCDevTools.MOD_ID)
+@Mod.EventBusSubscriber(modid = SekCDevTools.MOD_ID)
+public class SekCDevTools {
+
+    public static final String MOD_ID = "sekcdevtools";
+
+
+}
